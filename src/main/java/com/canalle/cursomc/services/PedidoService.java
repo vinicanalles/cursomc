b9ade4e87301606bd2a11668f67978319a7f074c
Vinicius Reia Canalles <vicanalles@gmail.com>
2020-11-20 16:49:48 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/canalle/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.canalle.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.canalle.cursomc.domain.Pedido;
+import com.canalle.cursomc.repositories.PedidoRepository;
+import com.canalle.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) throws ObjectNotFoundException {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
+	}
+}
