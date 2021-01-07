b1766fdd5a53f381106dbe37e521ffec12f43623
Vinicius Reia Canalles <vicanalles@gmail.com>
2021-01-07 10:27:40 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/canalle/cursomc/dto/ProdutoDTO.java
@@ -0,0 +1,48 @@
+package com.canalle.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.canalle.cursomc.domain.Produto;
+
+public class ProdutoDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private Double preco;
+	
+	public ProdutoDTO() {
+		
+	}
+	
+	public ProdutoDTO(Produto produto) {
+		this.id = produto.getId();
+		this.nome = produto.getNome();
+		this.preco = produto.getPreco();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
